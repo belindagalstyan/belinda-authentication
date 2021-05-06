b2dae3fdb76ea7a7d54f9dbc53df78780e5b002f
belindagalstyan <belindagalstyan@gmail.com>
2021-05-07 01:23:59 +0400

    gggg

--- server.js
+++ server.js
@@ -3,10 +3,27 @@ const app = express();
 
 const PORT = 5001;
 
+app.use(express.static('public'));
+// Middleware
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-	res.send("Hello");	
+	res.render("index");	
+})
+
+app.get("/surveybooks", (req, res) => {
+	res.render("surveybooks");	
+})
+
+app.get("/surveytennis", (req, res) => {
+	res.render("surveytennis");	
+})
+
+app.get("/admin", (req, res) => {
+	res.render("admin");	
 })
 
 app.listen(PORT, () => {
 	console.log("Server Started on:" + PORT);
 })
+
